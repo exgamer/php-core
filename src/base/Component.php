6303afeeb7457408353d80789e4ed87048089bec
exgamer@live.ru <1037213109sex>
2019-10-19 16:37:42 +0600

    1

--- src/base/Component.php
+++ src/base/Component.php
@@ -1,8 +1,6 @@
 <?php
 namespace concepture\php\core\base;
 
-use ReflectionException;
-
 /**
  * Base Component
  *
@@ -10,29 +8,5 @@ use ReflectionException;
  */
 abstract class Component extends BaseObject
 {
-    /**
-     * for use with logger
-     *
-     * @param $context
-     * @return mixed
-     * @throws ReflectionException
-     */
-    public function extendLogContext($context)
-    {
-        $context['class'] = $this->getShortName();
 
-        return $context;
-    }
-
-    /**
-     * for use with logger
-     *
-     * @param $message
-     * @return string
-     * @throws ReflectionException
-     */
-    public function extendLogMessage($message)
-    {
-        return  $message." [".$this->getShortName()."]";
-    }
 }
